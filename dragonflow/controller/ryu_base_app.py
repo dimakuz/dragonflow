d99c9e9fc302376c018fac819470861d8bbb39ec
Dima Kuznetsov <dima.kuznetsov@toganetworks.com>
2017-06-10 10:34:45 +0300

    Move apply_db_changes from NbApi to controller
    The code is very controller specific, and since we're using NbApi in
    other service now as well (BGP, Publisher), we should change it so
    each service can provide its own logic for handling updates.
    
    Change-Id: Ie5ef6f00f3e2739f7d9658a352b622b8f62083b2

--- dragonflow/controller/ryu_base_app.py
+++ dragonflow/controller/ryu_base_app.py
@@ -26,6 +26,7 @@ from ryu.ofproto import ofproto_v1_3
 from ryu import utils
 
 from dragonflow.controller import dispatcher
+from dragonflow.controller.common import constants
 
 
 LOG = log.getLogger(__name__)
@@ -100,8 +101,9 @@ class RyuDFAdapter(ofp_handler.OFPHandler):
         if not self.first_connect:
             # For reconnecting to the ryu controller, df needs a full sync
             # in case any resource added during the disconnection.
-            self.nb_api.db_change_callback(None, None, 'sync', 'full_sync')
-
+            self.nb_api.db_change_callback(None, None,
+                                           constants.CONTROLLER_REINITIALIZE,
+                                           None)
         self.first_connect = False
 
     def _send_port_desc_stats_request(self, datapath):
